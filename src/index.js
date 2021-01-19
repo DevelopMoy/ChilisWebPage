64cc7b0d319ae22a7321e4575bd86ca87c0c2ff2
moy <fabmoy1866@gmail.com>
2021-01-19 16:00:35 -0600

    Added bootstrap to Webpackk

--- src/index.js
+++ src/index.js
@@ -1,9 +1,2 @@
-// ARCHIVO PRINCIPAL DE NUESTRA APLICACION
-
-import {saludar} from "./js/components.js";
-import './styles.css';
-
-saludar("Peter Parker");
-const nam="Jaime Johnson";
-
-saludar(nam);
+import 'bootstrap'
+import 'bootstrap/dist/css/bootstrap.min.css';
