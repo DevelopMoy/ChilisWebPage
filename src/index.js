187d8c992d13651a38a0bcc987f50016ece30f68
moy <fabmoy1866@gmail.com>
2021-01-20 09:59:35 -0600

    Añadido responsive al panel de home

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import 'bootstrap'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/generalStyles.css'
+import './css/responsive.css'
 import {addCarouselEvents} from "./js/animateCarousel";
 
 addCarouselEvents();
