6f8aeb8638af7ae3407e28570c4711fcb2e18266
moy <fabmoy1866@gmail.com>
2021-01-20 11:02:37 -0600

    Carousel bug repaired

--- src/js/animateCarousel.js
+++ src/js/animateCarousel.js
@@ -1,12 +1,19 @@
 const totalImages = 5; // HOW MANY IMAGES THE CAROUSEL WILL HAVE, MINIMUM VALUE -> 1
 const carouselArea = document.querySelector("#areaImgSld");
 let imgIndex=1;
+const images = document.querySelectorAll(".carImg");
 
 export const addCarouselEvents = ()=>{
     addButtonsEvents();
 }
 
-const changeImg = (direction)=>{ // 0 FOR LEFT, 1 FOR RIGHT
+const changeImg = (direction)=>{
+    const imgIndex=goDirection(direction);
+    updateImage(imgIndex);
+}
+
+const goDirection =(direction)=>{
+    // 0 FOR LEFT, 1 FOR RIGHT
     if (direction){
         imgIndex++;
         imgIndex=imgIndex<=totalImages?imgIndex:1;
@@ -14,12 +21,25 @@ const changeImg = (direction)=>{ // 0 FOR LEFT, 1 FOR RIGHT
         imgIndex--;
         imgIndex=imgIndex>=1?imgIndex:totalImages;
     }
-    updateImage(imgIndex);
+    return imgIndex;
 }
 
 const updateImage = (imageNumber)=>{
-    const image = carouselArea.querySelector("img");
-    image.src=`./assets/productsImg/p${imageNumber}.png`;
+    const image = images[imageNumber-1];
+    imageNumber=imageNumber-2;
+    if (imageNumber<0){
+        imageNumber=totalImages-1;
+    }
+    if (imageNumber>=totalImages){
+        imageNumber=0;
+    }
+    const prevImage = images[imageNumber];
+    console.log("IMAGEN ACTUAL "+image.src);
+    console.log("IMAGEN ANTERIOR "+prevImage.src);
+    image.classList.remove("hiddeCar");
+    image.classList.add("showCar");
+    prevImage.classList.remove("showCar");
+    prevImage.classList.add("hiddeCar");
 }
 
 const addButtonsEvents = ()=>{
